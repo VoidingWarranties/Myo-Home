d187bd770e7d31ca220caa8880cdc93c50034497
Jordan Horwich <jordan@voidingwarranties.com>
2014-11-13 21:56:15 -0800

    Make pause/play and volume control more responsive
    Pause and play now respond on Gesture::none instead of
    Gesture::singleClick. Volume control now also responds on Gesture::none
    instead of Gesture::hold.
    
    Resolves #3

--- src/master_controller/MediaController.h
+++ src/master_controller/MediaController.h
@@ -19,42 +19,43 @@ class MediaController {
         arm_(myo::armRight),
         x_direction_(myo::xDirectionUnknown) {}
 
-  void togglePlay() {
-    media_manager_.togglePlay();
-  }
+  void togglePlay() { media_manager_.togglePlay(); }
 
   void onPose(myo::Myo* myo, PoseGestures<>::Pose pose) {
-    if (pose.gesture() == PoseGestures<>::Pose::Gesture::singleClick ||
-        pose.gesture() == PoseGestures<>::Pose::Gesture::doubleClick) {
+    if (pose.pose() != PoseGestures<>::Pose::fist) {
+      controlling_volume_ = false;
+    }
+    if (pose.pose() != PoseGestures<>::Pose::waveIn &&
+        pose.pose() != PoseGestures<>::Pose::waveOut) {
+      controlling_position_ = false;
+    }
+    if (pose.gesture() == PoseGestures<>::Pose::Gesture::none) {
       if (pose.pose() == PoseGestures<>::Pose::fingersSpread) {
         media_manager_.togglePlay();
         locker_p_->extendUnlock();
-      } else if (pose.pose() == PoseGestures<>::Pose::waveIn) {
+      } else if (pose.pose() == PoseGestures<>::Pose::fist) {
+        controlling_volume_ = true;
+        starting_rotation_ = rotation_;
+        std::cout << "Adjusting volume..." << std::endl;
+        locker_p_->extendUnlock();
+        myo->vibrate(myo::Myo::vibrationShort);
+      }
+    } else if (pose.gesture() == PoseGestures<>::Pose::Gesture::singleClick ||
+               pose.gesture() == PoseGestures<>::Pose::Gesture::doubleClick) {
+      if (pose.pose() == PoseGestures<>::Pose::waveIn) {
         media_manager_.previousTrack();
         locker_p_->extendUnlock();
       } else if (pose.pose() == PoseGestures<>::Pose::waveOut) {
         media_manager_.nextTrack();
         locker_p_->extendUnlock();
       }
-    } else if (pose.gesture() == PoseGestures<>::Pose::Gesture::hold &&
-               pose.pose() == PoseGestures<>::Pose::fist) {
-      controlling_volume_ = true;
-      starting_rotation_ = rotation_;
-      std::cout << "Adjusting volume..." << std::endl;
-      locker_p_->extendUnlock();
-      myo->vibrate(myo::Myo::vibrationShort);
-    } else if (last_pose_.gesture() == PoseGestures<>::Pose::Gesture::hold &&
-               last_pose_.pose() == PoseGestures<>::Pose::fist) {
-      controlling_volume_ = false;
-      myo->vibrate(myo::Myo::vibrationShort);
-    } else if (pose.gesture() == PoseGestures<>::Pose::Gesture::hold &&
-               (pose.pose() == PoseGestures<>::Pose::waveIn ||
-                pose.pose() == PoseGestures<>::Pose::waveOut)) {
-      controlling_position_ = true;
-      std::cout << "Seeking iTunes" << std::endl;
-      locker_p_->extendUnlock();
-    } else {
-      controlling_position_ = false;
+    } else if (pose.gesture() == PoseGestures<>::Pose::Gesture::hold) {
+      if (pose.pose() == PoseGestures<>::Pose::waveIn ||
+          pose.pose() == PoseGestures<>::Pose::waveOut) {
+        controlling_position_ = true;
+        std::cout << "Seeking iTunes" << std::endl;
+        locker_p_->extendUnlock();
+      }
     }
     last_pose_ = pose;
   }
@@ -67,7 +68,7 @@ class MediaController {
       if (x_direction_ != myo::xDirectionTowardWrist) {
         roll_diff *= -1;
       }
-      int volume_diff = roll_diff * 50; // Change this to your preference.
+      int volume_diff = roll_diff * 50;  // Change this to your preference.
       media_manager_.incrementVolumeBy(volume_diff);
       starting_rotation_ = rotation_;
     } else if (controlling_position_) {
@@ -88,7 +89,8 @@ class MediaController {
     rotation_ = quat;
   }
 
-  void onArmRecognized(myo::Myo* myo, myo::Arm arm, myo::XDirection x_direction) {
+  void onArmRecognized(myo::Myo* myo, myo::Arm arm,
+                       myo::XDirection x_direction) {
     arm_ = arm;
     x_direction_ = x_direction;
   }
@@ -97,7 +99,8 @@ class MediaController {
   LockController* locker_p_;
   ApplicationControlManager media_manager_;
   myo::Quaternion<float> rotation_, starting_rotation_;
-  bool controlling_volume_, controlling_position_, previous_controlling_position_;
+  bool controlling_volume_, controlling_position_,
+      previous_controlling_position_;
   PoseGestures<>::Pose last_pose_;
   myo::Arm arm_;
   myo::XDirection x_direction_;
